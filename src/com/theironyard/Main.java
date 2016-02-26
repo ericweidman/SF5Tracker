001227d9806f5e0ca4e6126cd7614f210070c27b
ericweidman <ericweidman@gmail.com>
2016-02-25 20:52:33 -0500

    Login no longer works with empty user inputs. Added logout button.

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,12 +1,14 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
 import java.util.HashMap;
 
 public class Main {
+    static HashMap<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -16,27 +18,61 @@ public class Main {
         Spark.get(
                 "/",
                 ((request, response) -> {
+                    User user = getUserFromSession(request.session());
                     HashMap m = new HashMap();
-                    return new ModelAndView(m, "login.html");
+                    if (user == null) {
+                        return new ModelAndView(m, "login.html");
+                    }
+                    else{
+                        m.put("name", user.userName);
+                        return new ModelAndView(m, "home.html");
+                    }
                 }),
                 new MustacheTemplateEngine()
         );
-
         Spark.post(
                 "create-user",
                 ((request, response) -> {
                     String userName = request.queryParams("userName");
                     String userPass = request.queryParams("userPass");
+                    User user = users.get(userName);
+                    if(userName.equals("")|| userPass.equals("")) {
+                        response.redirect("/");
+                    }
+                    else if(user == null){
+                        user = new User(userName, userPass);
+                        users.put(userName, user);
+                        user.userPass = userPass;
+                        Session session = request.session();
+                        session.attribute("userName", userName);
+                        response.redirect("/");
+                    } else if (userPass.equals(user.userPass)){
+                        response.redirect("/");
+                        Session session = request.session();
+                        session.attribute("userName", userName);
+                    }else{
+                        Spark.halt(403);
+                    }
+                    return "";
+                })
+        );
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
                     response.redirect("/");
                     return "";
                 })
         );
 
-
-
-
+    }
+    static User getUserFromSession(Session session){
+        String name = session.attribute("userName");
+        return users.get(name);
     }
 }
+
 
 
 //        Create login page.
